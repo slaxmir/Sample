8a34ada658b4f03f35615cf6f87217b9ce6c4488
slaxmir <slaxmi.rajaboina@gmail.com>
2016-10-10 11:42:44 +0530

    Sample
    New Sample Repo

---
+++ jssrc/android/generated/Home.js
@@ -0,0 +1,29 @@
+function addWidgetsHome() {
+    Home.setDefaultUnit(kony.flex.DP);
+    Home.add();
+};
+
+function HomeGlobals() {
+    Home = new kony.ui.Form2({
+        "addWidgets": addWidgetsHome,
+        "enabledForIdleTimeout": false,
+        "id": "Home",
+        "layoutType": kony.flex.FREE_FORM,
+        "needAppMenu": true
+    }, {
+        "displayOrientation": constants.FORM_DISPLAY_ORIENTATION_PORTRAIT,
+        "layoutType": kony.flex.FREE_FORM,
+        "padding": [0, 0, 0, 0],
+        "paddingInPixel": false
+    }, {
+        "footerOverlap": false,
+        "headerOverlap": false,
+        "menuPosition": constants.FORM_MENU_POSITION_AFTER_APPMENU,
+        "retainScrollPosition": false,
+        "titleBar": true,
+        "windowSoftInputMode": constants.FORM_ADJUST_PAN
+    });
+    Home.info = {
+        "kuid": "f2a1d30b0ab54c9fb9b5b2a5e66cc500"
+    };
+};
